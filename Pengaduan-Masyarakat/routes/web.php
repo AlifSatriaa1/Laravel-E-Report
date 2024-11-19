36a66c0098b83cc7ea5a36464fd92626b259404d
alifsatriamandala@gmail.com <macbook@Mewing.local>
2024-11-19 07:32:37 +0700

    first commit

---
+++ Pengaduan-Masyarakat/routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\User\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/', [UserController::class, 'index'])->name('ereporthub.index');
+
+
+Route::get('/register', [UserController::class, 'formRegister'])->name('ereporthub.formRegister');
